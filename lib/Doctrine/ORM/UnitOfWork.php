2375d835f023ac076c6e85dde9f866d4df7f00ea
Tiago Tix Carvalho <tiago.carvalho@gmail.com>
2021-07-13 15:35:51 +0100

    fixed warning on php74

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2662,7 +2662,7 @@ class UnitOfWork implements PropertyChangedListener
                         $class->reflFields[$field]->setValue($entity, null);
                         $this->originalEntityData[$oid][$field] = null;
 
-                        continue;
+                        break;
                     }
 
                     if ( ! isset($hints['fetchMode'][$class->name][$field])) {
